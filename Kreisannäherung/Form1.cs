3f61b20e9769eae2841a18fd89727533f5a6b83b
Kryptojude <68050323+Kryptojude@users.noreply.github.com>
2020-07-31 09:16:12 +0200

    Vector class exported to .cs-file

--- Kreisannäherung/Form1.cs
+++ Kreisannäherung/Form1.cs
@@ -10,48 +10,6 @@ using System.Windows.Forms;
 
 namespace Kreisannäherung
 {
-    class Vector
-    {
-        public double X;
-        public double Y;
-
-        public Vector(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-        public double GetMagnitude()
-        {
-            double magnitude = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
-            return magnitude;
-        }
-        public Vector GetUnitVector()
-        {
-            Vector unitVector = new Vector(X/GetMagnitude(), Y/GetMagnitude());
-            return unitVector;
-        }
-        public double GetDegrees()
-        {
-            double degrees = Math.Atan2(Y, X) * 180 / Math.PI;
-            return degrees;
-        }
-        public double GetRadians()
-        {
-            return GetDegrees() / 180 * Math.PI;
-        }
-        public PointF GetPointF()
-        {
-            return new PointF((float)X, (float)Y);
-        }
-        public void SetDegrees(double degrees)
-        {
-            double radians = degrees / 180 * Math.PI;
-            double magnitude = GetMagnitude();
-            X = Math.Cos(radians) * magnitude;
-            Y = Math.Sin(radians) * magnitude;
-        }
-    }
-
     public partial class Form1 : Form
     {
         float ts = 150; // tilesize
---
+++ Kreisannäherung/Vector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreisannäherung
+{
+    class Vector
+    {
+        public double X;
+        public double Y;
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double GetMagnitude()
+        {
+            double magnitude = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+            return magnitude;
+        }
+        public Vector GetUnitVector()
+        {
+            Vector unitVector = new Vector(X / GetMagnitude(), Y / GetMagnitude());
+            return unitVector;
+        }
+        public double GetDegrees()
+        {
+            double degrees = Math.Atan2(Y, X) * 180 / Math.PI;
+            return degrees;
+        }
+        public double GetRadians()
+        {
+            return GetDegrees() / 180 * Math.PI;
+        }
+        public PointF GetPointF()
+        {
+            return new PointF((float)X, (float)Y);
+        }
+        public void SetDegrees(double degrees)
+        {
+            double radians = degrees / 180 * Math.PI;
+            double magnitude = GetMagnitude();
+            X = Math.Cos(radians) * magnitude;
+            Y = Math.Sin(radians) * magnitude;
+        }
+    }
+}
